708092d0621cdaa6c1b2e58b4af48d221c00a0fe
Eren AVAR <eren.avar@hotmail.com>
2025-02-01 22:12:12 +0300

    create side navigaton component and nested layout on account area

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,3 +1,8 @@
-export default function Layout() {
-  return <div className="grid grid-cols-[16rem-1fr] h-full gap-12"></div>;
+export default function Layout({ children }) {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+      <div>Navigaton</div>
+      <div>{children}</div>
+    </div>
+  );
 }
