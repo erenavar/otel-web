00e816709f3cd1fccee275fedcb91bd7ce5853c1
Eren AVAR <eren.avar@hotmail.com>
2025-02-01 23:12:16 +0300

    create sign in/out buttons, add side navigation and change some displays

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,7 +1,9 @@
+import SideNavigation from "@/app/_components/SideNavigation";
+
 export default function Layout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
-      <div>Navigaton</div>
+      <SideNavigation />
       <div>{children}</div>
     </div>
   );
